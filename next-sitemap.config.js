a94a05d36fa4a517a4c426248730798d71dc1772
YobellN <yobelnugroho17@gmail.com>
2025-07-16 20:56:45 +0800

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://desatianyar.id',
+  siteUrl: process.env.SITE_URL || 'https://visit-tianyar.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'daily',
@@ -40,7 +40,7 @@ module.exports = {
       }
     ],
     additionalSitemaps: [
-      'https://desatianyar.id/sitemap.xml',
+      'https://visit-tianyar.com/sitemap.xml',
     ],
   },
   transform: async (config, path) => {
