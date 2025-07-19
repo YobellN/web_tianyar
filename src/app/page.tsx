0b5ccfb6e2b784d6b7e4113845816387e2bbaa5e
YobellN <yobelnugroho17@gmail.com>
2025-07-19 22:58:10 +0800

    tes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Metadata } from 'next';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import VideoProfile from '@/components/VideoProfile';
