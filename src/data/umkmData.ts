2e1293f39652fb608b728081a6f99a8b8843e1f4
YobellN <yobelnugroho17@gmail.com>
2025-07-20 16:54:23 +0800

    add umkm data

--- src/data/umkmData.ts
+++ src/data/umkmData.ts
@@ -17,11 +17,11 @@ export const umkmData: UMKMItem[] = [
     alamat: "https://maps.app.goo.gl/XySUcqUg4jZVLxMP9",
   },
   {
-    id: "arak-bu-made",
-    nama: "Minuman Arak Bu Made",
-    deskripsi: "Tas, topi, dan dekorasi dari anyaman bambu dan pandan berkualitas",
-    gambar: "/umkm/arak_bu_made.JPG",
+    id: "arak-pak-made-tianyar",
+    nama: "Minuman Arak Pak Made Tianyar",
+    deskripsi: "Minuman khas Bali dengan cita rasa khas Bali",
+    gambar: "/umkm/arak_pak_made.JPG",
     kontak: "+62 813-4567-8901",
-    alamat: "Banjar Tianyar Kaja",
+    alamat: "https://maps.app.goo.gl/6X7ECbjZpka2hP296",
   }
 ];
